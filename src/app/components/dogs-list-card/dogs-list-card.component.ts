0e942d44041845924addf6e3a0832569cc931211
Motjiang <motjiangbrilliant@gmail.com>
2025-02-06 13:53:15 +0200

    Added css

--- src/app/components/dogs-list-card/dogs-list-card.component.ts
+++ src/app/components/dogs-list-card/dogs-list-card.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Dog } from '../../dog';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dogs-list-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   template: `
-    <p>
-      dogs-list-card works!
+    <article class="pet-card">
+    <img class="pet-img" src="{{dog.photoUrl}}" alt="Photo of {{dog.name}}">
+    <p class="pet-headline">Meet <span class="pet-name">{{dog.name}}</span></p>
+    <p class="pet-description"> 
+      <span class="pet-name">{{dog.ownerName}}</span> wants you to know this about {{dog.name}}:
+      {{dog.description}}
     </p>
+    <p class="pet-learn-more"><a href="/details/{{index}}">Learn More</a></p>
+  <article>
   `,
   styleUrl: './dogs-list-card.component.css'
 })
-export class DogsListCardComponent {
+export class DogsListCardComponent implements OnInit {
+  @Input() dog!: Dog;
+  @Input() index!: Number;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
